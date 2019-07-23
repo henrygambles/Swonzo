32b876f169a33bf96ce9926cb03070b67c52aac1
henrygambles <gambles.henry2@gmail.com>
2019-07-23 15:05:28 +0100

    Hardcoded Tableview & plenty of messy code

--- Swonzo/SwonzoClient.swift
+++ Swonzo/SwonzoClient.swift
@@ -187,6 +187,67 @@ class SwonzoClient {
     
     
     
+    func WhackIt() {
+        
+        let token = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYiI6Ikh6bSswNFBpeWw2YUQ0blExKzZkIiwianRpIjoiYWNjdG9rXzAwMDA5bDZOYkpsRXgyaWhESk05a1AiLCJ0eXAiOiJhdCIsInYiOiI1In0.yhucPmR8QCme5lpG7iHlrkAZHeVCLiOBru9s7Ag4C4ZfWWY_iKQXy-dBMCaiKtmbhuB-HKmNrvRWTHic7D06ng"
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer " + token
+        ]
+        
+        
+        Alamofire.request("https://api.monzo.com/accounts",
+                          encoding:  URLEncoding.default,
+                          headers: headers).responseJSON { response in
+                            if let error = response.error {
+                                //                            self.homeView.text = "hey there"
+                            } else if let jsonArray = response.result.value as? [[String: Any]] {
+                                //                            self.homeView.text = "whattup"
+                            } else if let jsonDict = response.result.value as? [String: Any] {
+                                
+                                
+                                
+                                do {
+                                    
+                                    let json = try JSON(data: response.data!)
+                                    let account_number = json["accounts"][0]["account_number"].string
+                                    let acc_id = json["accounts"][0]["id"].string
+                                    let sort_code = json["accounts"][0]["sort_code"].string
+                                    let first_name = json["accounts"][0]["owners"][0]["preferred_first_name"].string
+                                    let full_name = json["accounts"][0]["owners"][0]["preferred_name"].string
+                                    let user_id = json["accounts"][0]["owners"][0]["user_id"].string
+                                    //                                    let balance = json[0]["balance"][0].string
+                                    //                                    let balance1 = json[0][0]["balance"].string
+                                    //                                    let balance2 = json["balance"][0][0]["balance"].string
+                                    //                                    let balance3 = json["balance"][0]["balance"].string
+                                    
+                                    
+                                    //                                    self.homeView.text = "Hi " + first_name! + "! Welcome to Swonzo.\n\nYour account number is:\n" + account_number! + "\n\nAnd your sort code is:\n" + sort_code! + "\n\nHope it helps!"
+                                    
+                                    
+                                   
+                                    print("TESTING")
+                                    print(full_name)
+                                    print(acc_id)
+                                    print(user_id)
+                                    print(account_number)
+                                    print(sort_code)
+                                    print(first_name)
+                                    
+                                    
+                                    
+                                } catch {
+                                    print("JSON Parsing error:", error)
+                                }
+                                
+                            }
+        }
+    }
+
+    
+    
+    
+    
     
     func hitIt(_ url: URL, completion: @escaping WebServiceResponse) {
         
--- Swonzo/ViewConrollers/HomeViewController.swift
+++ Swonzo/ViewConrollers/HomeViewController.swift
@@ -18,13 +18,89 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        self.homeView.text = "lel"
+
+        typealias WebServiceResponse = ([[String: Any]]?, Error?) -> Void
+
+        func initialRequest() {
+
+            let token = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYiI6Ikh6bSswNFBpeWw2YUQ0blExKzZkIiwianRpIjoiYWNjdG9rXzAwMDA5bDZOYkpsRXgyaWhESk05a1AiLCJ0eXAiOiJhdCIsInYiOiI1In0.yhucPmR8QCme5lpG7iHlrkAZHeVCLiOBru9s7Ag4C4ZfWWY_iKQXy-dBMCaiKtmbhuB-HKmNrvRWTHic7D06ng"
+
+            let headers: HTTPHeaders = [
+                "Authorization": "Bearer " + token
+            ]
+
+
+            Alamofire.request("https://api.monzo.com/accounts",
+                              encoding:  URLEncoding.default,
+                              headers: headers).responseJSON { response in
+                                if let error = response.error {
+                                    //                            self.homeView.text = "hey there"
+                                } else if let jsonArray = response.result.value as? [[String: Any]] {
+                                    //                            self.homeView.text = "whattup"
+                                } else if let jsonDict = response.result.value as? [String: Any] {
+
+
+
+                                    do {
+
+                                        let json = try JSON(data: response.data!)
+                                        let account_number = json["accounts"][0]["account_number"].string
+                                        let acc_id = json["accounts"][0]["id"].string
+                                        let sort_code = json["accounts"][0]["sort_code"].string
+                                        let first_name = json["accounts"][0]["owners"][0]["preferred_first_name"].string
+                                        let full_name = json["accounts"][0]["owners"][0]["preferred_name"].string
+                                        let user_id = json["accounts"][0]["owners"][0]["user_id"].string
+                                        //                                    let balance = json[0]["balance"][0].string
+                                        //                                    let balance1 = json[0][0]["balance"].string
+                                        //                                    let balance2 = json["balance"][0][0]["balance"].string
+                                        //                                    let balance3 = json["balance"][0]["balance"].string
+
+
+                                        //                                    self.homeView.text = "Hi " + first_name! + "! Welcome to Swonzo.\n\nYour account number is:\n" + account_number! + "\n\nAnd your sort code is:\n" + sort_code! + "\n\nHope it helps!"
+
+
+
+                                        print("TESTING")
+                                        print(full_name)
+                                        print(acc_id)
+                                        print(user_id)
+                                        print(account_number)
+                                        print(sort_code)
+                                        print(first_name)
+                                        self.homeView.text = "Hi " + first_name! + "!\n\nWelcome to Swonzo.\n\nA hub for all your financial needs.\n\nYour account number is:\n" + account_number! + "\n\nYour sort code is:\n" + sort_code! + "\n\nAnd your account id is:\n" + acc_id! + "\n\n!"
+
+
+
+                                    } catch {
+                                        print("JSON Parsing error:", error)
+                                    }
+
+                                }
+            }
+        }
         
         let APIRequest = SwonzoClient()
         
+        initialRequest()
         APIRequest.getAccounts()
         APIRequest.getBalance()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
 //        guard let urlToExecute = URL(string: "https://api.monzo.com/balance") else {
 //            return
